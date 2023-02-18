54d68fc1838393f6026d3ea1875235c6627c43f2
krishna-ship-it <krish36pandey@gmail.com>
2023-02-18 17:20:08 +0530

    initial

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-
+const path = require("path");
 app.use(cors());
+app.use(express.static("build"));
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 app.use(function (req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "*");
 
